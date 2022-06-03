eaa267eed82c4458fa11b6e67d02cee4c45ca177
castelll0009 <45212708+castelll0009@users.noreply.github.com>
2022-06-03 11:25:07 -0500

    Update appacto.js

--- js/appacto.js
+++ js/appacto.js
@@ -35,9 +35,9 @@ $(document).ready(function(){
 	}); 
 });
 
-function CambiarIcono(){
+function CambiarIcono(){ //cambia iconos
     var arrayIconos= new Array('icono-estudiante.png', 'icono-gym.png','icono-agricultor.png','icono-mujeres.png','icono-interculturalidad.png','logo.png','salud.png','sampedro.png','cafe.png','arte.png','baile.gif','condor.png','animales.png','colibri.png','turismo.png','pacto-png.gif','mariposa-org.gif','loro.png','atrapa-sueños.png','indio.png','civil.png','programador.png','profesora.png','geografo.png','musico.png','abogado.png','lenguas.png','logo1.png','logo2.png','afro.png');
-    var stringRutaIcono =(arrayIconos[getRandomInt(0,arrayIconos.length)]);
+    var stringRutaIcono =(arrayIconos[getRandomInt(0,arrayIconos.length)]); 
     $("#etiqueta-icono").attr("src","imgs/"+stringRutaIcono);
     var etiquetaIcono= $("#etiqueta-icono").html();
 }
